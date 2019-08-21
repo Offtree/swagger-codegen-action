234927619a91da8952abf7299b1755f2909995d9
OffTree <davidtrowe@gmail.com>
2019-08-21 12:05:26 -0500

    Add autocommit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-sh -c "/usr/bin/java -jar /opt/swagger-codegen-cli/swagger-codegen-cli.jar generate -i http://petstore.swagger.io/v2/swagger.json -l ruby -o ./"
+sh -c "/usr/bin/java -jar /opt/swagger-codegen-cli/swagger-codegen-cli.jar generate -i http://petstore.swagger.io/v2/swagger.json -l ruby -o ./generated-example"
