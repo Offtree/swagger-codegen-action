9b0628ac81d7fb3e5876e6c2b5a0f76bc3c8e6e9
OffTree <davidtrowe@gmail.com>
2019-08-21 11:41:12 -0500

    update swagger codegen call

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-sh -c "swagger-codegen generate -i http://petstore.swagger.io/v2/swagger.json -l ruby -o ./"
+sh -c "swaggerapi/swagger-codegen-cli  generate -i http://petstore.swagger.io/v2/swagger.json -l ruby -o ./"
